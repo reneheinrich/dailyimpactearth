1738a6dda4e2dc3f3b2d364ff0df69ec7db803b7
Tom <thomas.weckermann@hotmail.com>
2020-03-23 22:06:14 +0100

    Work2

--- index.php
+++ index.php
@@ -284,10 +284,10 @@ echo "<!DOCTYPE html>\n"
                 <div class="contentcontainer">
                     <h2>Während der Arbeit</h2>
                     <div class="fact">
-                        Du schwimmst in Arbeit, aber im Pazifik schwimmt eine Plastikinsel 4 Mal so groß wie Deutschland.
+                    Während du in Arbeit schwimmst, schwimmt im Pazifik eine Plastikinsel 4 Mal so groß wie Deutschland
                     </div>
                     <div class="cta-products">
-                        So bewältigst du den Office-Alltag nachhaltiger
+                    Die auch als “Great Pacific Garbage Patch” bekannte Insel umfasst ca. 1.6 millionen Quadratkilometer an Plastikmüll. Mehr dazu findest du <a href="https://www.nature.com/articles/s41598-018-22939-w" target="_blank">hier</a>.
                     </div>
                     <div class="owl-carousel">
                         <?php
@@ -350,12 +350,21 @@ echo "<!DOCTYPE html>\n"
                 <div class="contentcontainer">
                     <h2>Während der Arbeit</h2>
                     <div class="fact">
-                        Neuer fact
+                    Jährlich fallen in Deutschland pro Kopf mehr als 20kg Elektroschrott an
                     </div>
-                    <?php
+                    <div class="cta-products">
+                    Speziell die als "Seltene Erden" bekannten Edelmedalle stellen sich im Abbau als besonders problematisch für die Umwelt dar. Diese überwiegend in China abgebauten Metalle, lassen sich leider nur bedingt effizient recyclen. Norwegen ist übrigens Elektroschrott-Weltmeister, dicht gefolgt von anderen westlichen und europäischen Ländern. Im Vergleich fallen in Afrika durchschnittlich pro Kopf nur ca. 2kg an. Mehr zu diesem Thema kannst du <a href="https://www.br.de/themen/wissen/seltene-erden-metalle-smartphones-china-100.html" target="_blank"> hier </a>und <a href="https://www.sueddeutsche.de/wissen/elektroschrott-globaler-berg-an-elektromuell-waechst-rasant-1.3789993" target="_blank">hier</a> lesen.      
+                   
+                    </div>
+                    <div>
+                    <img class="graph-vertical" src="elektroschrott.svg" >
+                    </div>
+                   
+                    
+                   <?php
                         share(
                             "work2",
-                            "Wusstest du, dass im Pazifik eine Plastikinsel 4 Mal so groß wie Deutschland schwimmt?",
+                            "Wusstest du, dass in Deutschland pro Kopf jährlich mehr als 20kg Elektroschrott anfallen?",
                             "createimpact,impact,noplastic"
                         );
                     ?>
