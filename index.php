6623d9b3f7533f9c72fbff1a9ecd4f61f763e798
Tom <thomas.weckermann@hotmail.com>
2020-03-24 18:34:59 +0100

    Pause 2

--- index.php
+++ index.php
@@ -380,27 +380,7 @@ echo "<!DOCTYPE html>\n"
                     </div>
                     <div class="cta-products">
                     Das ist in etwa der CO2 Ausstoß von 8 Kurzstreckenflügen. Natürlich möchte nicht jeder komplett vegan oder vegetarisch leben. Aber auch hin und wieder auf Alternativprodukte zurückzugreifen kann einen Unterschied machen. Mehr dazu <a href="https://utopia.de/vegan-treibhausgase-co2-137342/"> hier.</a>
-                    </div>        
-                    <?php
-                        share(
-                            "break",
-                            "",
-                            ""
-                        );
-                    ?>
-                </div>
-            </div>
-        </div>
-        <div id="x_break2" vs-anchor="break2" class="mainview">
-            <div class="darkbg">
-                <div class="contentcontainer">
-                    <h2>In der Pause</h2>
-                    <div class="fact">
-                        Veganer erzeugen mit ihrer Ernährung 40% weniger CO<sup>2</sup> pro Jahr als Fleischesser.
-                    </div>
-                    <div class="cta-products">
-                        Teste diese veganen Produkte und plastikfreien Alternativen
-                    </div>
+                    </div>     
                     <div class="owl-carousel">
                         <?php
                         amzn(
@@ -434,12 +414,76 @@ echo "<!DOCTYPE html>\n"
                             "100% Bio Erdnüsse ohne Zusätze wie Palmöl, Salz oder Zucker."
                         );
                         ?>
+                    </div>   
+                    <?php
+                        share(
+                            "break",
+                            "",
+                            ""
+                        );
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div id="x_break2" vs-anchor="break2" class="mainview">
+            <div class="darkbg">
+                <div class="contentcontainer">
+                    <h2>In der Pause</h2>
+                    <div class="fact">
+                        Die Erzeugung einer einzigen Tasse Kaffee verschlingt 140l Wasser
+                    </div>
+                    <div class="cta-products">
+                    Aber auch die massive Abholzung im Rahmen der Kaffeeproduktion wird zunehmend zur Gefahr für unsere
+                    Regenwälder. Die durch den Klimawandel bedingte Verschiebung der Anbaugebiete führt dazu, dass die immer steigende Nachfrage
+                    nach Kaffee künftig nur durch weitere Rhodung der Regenwälder zu decken sein wird. 
+                    <bold><a href="https://www.br.de/radio/bayern1/inhalt/experten-tipps/umweltkommissar/kaffee-kapseln-pads-umwelt-100.html" target="_blank">Hier</a></bold> und 
+                    
+                   <a href="https://www.tagesanzeiger.ch/wissen/natur/fuer-unseren-taeglichen-kaffee-wird-der-regenwald-sterben/story/11876527" target="_blank">hier</a> findest du mehr zu diesen Themen.
+
+                    </div>
+                    <div class="owl-carousel">
+                        <?php
+               
+                        amzn(
+                            "33JOnno",
+                            "img/kaffee-1.jpg",
+                            "Kompostierbare Bio Kapseln",
+                            "Kaffeebohnen aus Kleinbauernkooperativen mit kontrolliert ökolog. nachhaltigem Anbau "
+                        );
+                        amzn(
+                            "3ahAQWE",
+                            "img/kaffee-2.jpg",
+                            "Bio Fairtrade Kaffeebohnen",
+                            "Bohnen von kleinen, unabhängigen Partner Farmen. Umweltschonend angebaut und zert. mit Fairtrade- und Bio-Siegel."
+                        );
+
+                        amzn(
+                            "3drcQT2",
+                            "img/kaffee-3.jpg",
+                            "UTZ-zertifizierte Kaffeebohnen",
+                            "Hochwertige Bohnen aus umweltfreundlicher Produktion"
+                        );
+
+                        amzn(
+                            "38wPKaJ",
+                            "img/proteinriegel.jpg",
+                            "Riegel aus Grillenmehl",
+                            "Vegane und proteinreiche Snacks aus Grillenmehl."
+                        );
+              
+                        amzn(
+                            "37sWlSd",
+                            "img/erdnuss.jpg",
+                            "Bio Peanut Butter",
+                            "100% Bio Erdnüsse ohne Zusätze wie Palmöl, Salz oder Zucker."
+                        );
+                        ?>
                     </div>
                     <?php
                         share(
                             "break2",
-                            "Wusstest du, dass Veganer mit ihrer Ernährung 40% weniger CO2 pro Jahr erzeugen als Fleischesser?",
-                            "createimpact,impact,govegan"
+                            "Wusstest du, dass die Erzeugung einer einzigen Tasse Kaffee 140 Liter Wasser verschlingt?",
+                            "createimpact,impact,kaffee"
                         );
                     ?>
                 </div>
