aad1e3a5f83e69005c222cc2be6ecda3c555bf93
René Heinrich <rene@halphbaqed.com>
2020-04-03 14:36:39 +0200

    gotowork separated

---
+++ gotowork.php
@@ -0,0 +1,21 @@
+        <div id="commute" vs-anchor="gotowork" class="mainview">
+            <div class="darkbg">
+                <div class="contentcontainer">
+                    <h2>Ab zur Arbeit</h2>
+                    <div class="fact">
+                        Ein Pendler stößt auf seinem durchschnittlichen Arbeitsweg pro Tag 4,2kg CO2 aus 
+                    </div>
+                    <div class="cta-products">
+                        In der Graphik siehst du wie viel Gramm CO2 pro Personenkilometer ausgestoßen werden. Mehr Informationen findest du <a href="https://www.focus.de/wissen/klima/fleisch-essen-ist-besonders-schlecht-foehnen-waschen-zaehne-putzen-so-viel-co2-produzieren-sie-an-einem-normalen-tag_id_10628671.html">hier</a>.
+                    </div>
+                    <img class="graph" src="verkehr.svg">
+                    <?php
+                        share(
+                            "gotowork",
+                            "Wusstest du, dass ein Pendler auf seinem durchschnittlichen Arbeitsweg pro Tag 4,2kg CO2 ausstößt?",
+                            "createimpact,impact,cleancommute"
+                        );
+                    ?>
+                </div>
+            </div>
+        </div>
--- index.php
+++ index.php
@@ -214,27 +214,7 @@ echo "<!DOCTYPE html>\n"
                 </div>
             </div>
         </div>
-        <div id="commute" vs-anchor="gotowork" class="mainview">
-            <div class="darkbg">
-                <div class="contentcontainer">
-                    <h2>Ab zur Arbeit</h2>
-                    <div class="fact">
-                        Ein Pendler stößt auf seinem durchschnittlichen Arbeitsweg pro Tag 4,2kg CO2 aus 
-                    </div>
-                    <div class="cta-products">
-                        In der Graphik siehst du wie viel Gramm CO2 pro Personenkilometer ausgestoßen werden. Mehr Informationen findest du <a href="https://www.focus.de/wissen/klima/fleisch-essen-ist-besonders-schlecht-foehnen-waschen-zaehne-putzen-so-viel-co2-produzieren-sie-an-einem-normalen-tag_id_10628671.html">hier</a>.
-                    </div>
-                    <img class="graph" src="verkehr.svg">
-                    <?php
-                        share(
-                            "gotowork",
-                            "Wusstest du, dass ein Pendler auf seinem durchschnittlichen Arbeitsweg pro Tag 4,2kg CO2 ausstößt?",
-                            "createimpact,impact,cleancommute"
-                        );
-                    ?>
-                </div>
-            </div>
-        </div>
+        <?php require("gotowork.php"); ?>
         <?php require("gotowork2.php"); ?>
         <?php require("work.php"); ?>
         <div id="x_work2" vs-anchor="work2" class="mainview">
