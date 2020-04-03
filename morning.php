7795684028fd576fe1ee6ef3c680421442ae6d1f
René Heinrich <rene@halphbaqed.com>
2020-04-03 14:38:44 +0200

    morning separated

---
+++ morning.php
@@ -0,0 +1,47 @@
+        <div id="x_morning" vs-anchor="morning" class="mainview">
+            <div class="darkbg">
+                <div class="contentcontainer">
+                    <h2>Am Morgen</h2>
+                    <div class="fact">
+                       450 Jahre dauert es bis sich eine einfache Plastikflasche zersetzt
+                    </div>
+                    <div class="cta-products">
+                    Gerade unser Badezimmer ist besonders voll mit Plastikprodukten: Zahnbürsten, Cremes, Haargels, Wattestäbchen und vieles mehr. Dies macht es fast unmöglich plastikfrei in den Tag zu starten. <a href="https://www.zeit.de/2015/26/plastikmuell-nordsee-recycling-umweltschutz/komplettansicht" target="_blank"> Mehr Infos</a>.</div>
+                    <div class="owl-carousel">
+                        <?php
+                            amzn(
+                                "2uHbVMK", 
+                                "img/zahnbuerste.jpg", 
+                                "Bambus Zahnbürsten",
+                                "Unsere Bambus Zahnbürsten sind aus Bio-Moso-Bambus hergestellt, zu 100% biologisch abbaubar und umweltfreundlich."
+                            );
+                            amzn(
+                                "2OYddd3", 
+                                "img/zahnpasta.jpg", 
+                                "Kokosnuss Aktivkohle Zahnpasta",
+                                "Natürliche Zahnpasta ohne Kunststoffe, tierischen oder schädlichen Inhaltsstoffen und ist vegan und aus recycelbaren Tuben."
+                            );
+                            amzn(
+                                "38x9fQ9", 
+                                "img/wattestaebchen.jpg", 
+                                "Bambus Wattestäbchen",
+                                "Wattestäbchen aus Bambus und Baumwolle sind zu 100% biologisch abbaubar und werden nur aus organisch angebautem Bambus gefertigt."
+                            );
+                            amzn(
+                                "2Hv6uTE", 
+                                "img/zahnseide.jpg", 
+                                "Kompostierbare Zahnseide",
+                                "Die ökologische und ressourcenschonende Alternative zu herkömmlicher Nylon-Zahnseide wird aus den Kokons der Seidenraupe gewonnen."
+                            );
+                        ?>
+                    </div>
+                    <?php
+                        share(
+                            "morning",
+                            "Wusstest du, dass eine Plastikflasche bis zu 450 Jahre braucht bis sie sich zersetzt?",
+                            "createimpact,impact,noplastic"
+                        );
+                    ?>
+                </div>
+            </div>
+        </div>
---
+++ morning2.php
@@ -0,0 +1,60 @@
+        <div id="x_morning2" vs-anchor="morning2" class="mainview">
+            <div class="darkbg">
+                <div class="contentcontainer">
+                    <h2>Am Morgen</h2>
+                    <div class="fact">
+                        Deutschland verbraucht so viel Papier wie die Kontinente Afrika und Südamerika zusammen
+                    </div>
+                    <div class="cta-products">
+                        Fast jeder zweite industriell gefällte Baum weltweit wird zu Papier verarbeitet - Zeitungen, Zeitschriften, Geschenkpapier, Verpackungen, Küchentücher oder Toilettenpapier. Mehr Informationen findest du <a href="https://www.wwf.de/themen-projekte/waelder/papierverbrauch/zahlen-und-fakten/">hier</a>.
+                    </div>
+                    <div class="owl-carousel">
+                        <?php
+                            amzn(
+                                "2uHdNFg",
+                                "img/bambushandtuecher.jpg",
+                                "Bambus Küchenrolle",
+                                "Jeden Tag werden 51.000 Tonnen Bäume für Küchentücher gefällt. Mit der Küchenrolle wirkst du der Abholzung der Wälder entgegen."
+                            );
+                            amzn(
+                                "38vkFEk", 
+                                "img/müllbeutel.jpg", 
+                                "Kompostierbare Müllsäcke",
+                                "100% heim kompstierbare und biologisch abbaubare Müllsäcke."
+                            );
+                            amzn(
+                                "3bJct5p", 
+                                "img/handbrause.jpg", 
+                                "Wassersparender Duschkopf",
+                                "3 Wege Funktion Duschkopf reinigt Wasser mit Ionenfiltern und Mineralkugeln und führt zu bis zu 30% Wasserersparnis."
+                            );
+                            amzn(
+                                "2u6F5oa", 
+                                "img/haarshampoo.jpg", 
+                                "Mikroplastikfreies Haarshampoo",
+                                "Handgemachtes, natürliches und veganes Haarshampoo ohne Mirkoplastik."
+                            );
+                            amzn(
+                                "3bJct5p", 
+                                "img/zahnpasta1.jpg", 
+                                "Aktivkohlezahnpasta aus Kokos",
+                                "Es besteht zu 100% aus organischer Aktivkohle, aus der Kokosnuss, mit maximaler Schonwirkung für Zähne, Zahnfleisch und Körper."
+                            );
+                            amzn(
+                                "37yONx0", 
+                                "img/kokosschwamm.jpg", 
+                                "Kokosschwamm",
+                                "Kokosfasern sind umweltschonend und bilogisch abbaubar!"
+                            );
+                        ?> 
+                    </div>
+                    <?php
+                        share(
+                            "morning2",
+                            "Wusstest du, dass Deutschland so viel Papier wie die Kontinente Afrika und Südamerika zusammen verbraucht?",
+                            "createimpact,impact,reducewaste"
+                        );
+                    ?>
+                </div>
+            </div>
+        </div>
