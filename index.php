0e3274ec66dc5aa09e2084e5c3691ee2668d3d8c
Harald <h.federspiel@gmail.com>
2020-04-03 12:31:52 +0200

    changes

--- index.php
+++ index.php
@@ -219,7 +219,7 @@ echo "<!DOCTYPE html>\n"
                 <div class="contentcontainer">
                     <h2>Ab zur Arbeit</h2>
                     <div class="fact">
-                        Wusstest du, dass ein Pendler auf seinem durchschnittlichen Arbeitsweg pro Tag 4,2kg CO2 ausstoßt?. 
+                        Ein Pendler stößt auf seinem durchschnittlichen Arbeitsweg pro Tag 4,2kg CO2 aus 
                     </div>
                     <div class="cta-products">
                         In der Graphik siehst du wie viel Gramm CO2 pro Personenkilometer ausgestoßen werden. Mehr Informationen findest du <a href="https://www.focus.de/wissen/klima/fleisch-essen-ist-besonders-schlecht-foehnen-waschen-zaehne-putzen-so-viel-co2-produzieren-sie-an-einem-normalen-tag_id_10628671.html">hier</a>.
