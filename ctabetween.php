ec6991596d03e5a91079a02cbe6df76cb46bb6a3
Alex <schuh.alex1@gmail.com>
2020-04-06 19:42:04 +0200

    share in between

---
+++ ctabetween.php
@@ -0,0 +1,19 @@
+<div id="x_ctabetween" vs-anchor="ctabetween" class="mainview lastpage">
+            <div class="darkbg lastpage">
+                    <div class="contentcontainer">
+                        <h2>Newsletter</h2>
+                        <div class="fact lastpage">
+                           Wir hoffen du findest die Website bis jetzt spannend. <br>Lass uns in Kontakt bleiben! 
+                        </div>
+                        <a href="newsletter.html" target="_blank" class="end-button">Newsletter anmelden</a>
+                        <div class="calltoshare">Wenn auch dir die Umwelt wichtig ist, teile uns mit deinen Freunden!</div>
+                        <?php
+                        share(
+                            "home",
+                            "Wusstest du, dass jeder Mensch pro Woche Plastik in der Menge von einer Kreditkarte zu sich nimmt?",
+                            "createimpact,impact,noplastic"
+                        );
+                    ?>
+                    </div>
+                </div>
+        </div>
