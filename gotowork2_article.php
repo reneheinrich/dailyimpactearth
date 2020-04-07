5e736787f2c2db66834f999c60b4d314304a7696
René Heinrich <rene@halphbaqed.com>
2020-04-07 11:03:42 +0200

    article draft for go to work 2

---
+++ gotowork2_article.php
@@ -0,0 +1,24 @@
+<h1>Feinstaub trägt europaweit zu einem Drittel der Asthmafälle bei.</h1>
+<span>Autor: René</span>
+<p>
+Der tägliche Straßenverkehr ist nach einer <a href="https://www.stmuv.bayern.de/themen/luftreinhaltung/verunreinigungen/feinstaub/emissionenpm10.htm">Studie des Emissionskatasters Bayern und des Landesamts für Umwelt</a> einer der Hauptverurusacher von Feinstaub.
+<br />[Grafik aus der Quelle]<br />
+Eine Möglichkeit die Feinstaubemissionen in Städten zu verringern ist der Umstieg auf Elektromobilität.
+Es ist erwiesen, dass aktuelle Elektrofahrzeuge am Markt im Vergleich zu Fahrzeugen mit Verbrennungsmotoren in 
+allen Klimagesichtspunkten besser abschneiden.
+Auch hinsichtlich des CO2-Fußabdrucks steht ein Elektroauto im Schnitt 16% besser da als moderne und als besonders sparsam
+geltende Dieselfahrzeuge. Bei einem Benziner sind es sogar 27% weniger CO2.<br/>
+Und nicht immer muss es ein Auto sein. Pendler fahren überwiegend alleine zur Arbeit. Hier lohnt sich 
+oft der Umstieg auf Ebikes und Elektroroller. Wer besonders cool durch die City navigieren möchte, kann auch auf 
+ein trendiges Elektro-Longboard umsteigen. Ohne Mühe düst man hier mit 30km/h durch die Stadt und 
+entlastet Straßen. Die Suche nach einem Parkplatz entfällt praktischerweise auch.
+Wer hier sicher unterwegs sein will, sollte natürlich auf einen passenden Helm nicht verzichten.
+</p>
+<h2>Quellen</h2>
+<ul>
+<li><a href="https://www.medical-tribune.de/medizin-und-forschung/artikel/oft-ist-feinstaub-verantwortlich-fuer-asthma/" target="_blank">Feinstaub verantwortlich für Asthma</a></li>
+    <li><a href="https://www.stmuv.bayern.de/themen/luftreinhaltung/verunreinigungen/feinstaub/emissionenpm10.htm">Studie über Ursachen von Feinstaubemssionen des Emissionskatasters Bayern und des Landesamts für Umwelt</a></li>
+    <li><a href="https://www.bmu.de/fileadmin/Daten_BMU/Pools/Broschueren/elektroautos_bf.pdf" target="_blank">BMU Elektroautos</a></li>
+    <li>Quelle Pendler alleine unterwegs</li>
+</ul>
+
