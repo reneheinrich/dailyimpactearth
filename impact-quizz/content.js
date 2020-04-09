dcaa364942cb7a7dea37b48ce44e0d880a082b68
Tom <thomas.weckermann@hotmail.com>
2020-04-09 17:51:15 +0200

    daily impact quizz

---
+++ impact-quizz/content.js
@@ -0,0 +1,75 @@
+let quizzContent =
+
+[
+   {
+        "frage": "Welches dieser Verkehrsmittel stößt am wenigsten CO2 pro Personenkilometer aus?",
+        "a1": "U-Bahn",
+        "a2": "Fernbus",
+        "a3": "Auto",
+        "korrekt": "2",
+        "korrekteAntwort": "Fernbus"
+    },
+    {
+        "frage": "Wie viel Liter Wasser verursacht die Produktion einer einzigen Tasse Kaffee?",
+        "a1": "140l",
+        "a2": "87l",
+        "a3": "19l",
+        "korrekt": "1",
+        "korrekteAntwort": "140 Liter"
+
+    },
+    {
+        "frage": "Wie viel Kilogramm Fleisch werden in Deutschland pro Kopf und Jahr konsumiert?",
+        "a1": "15kg",
+        "a2": "60kg",
+        "a3": "27kg",
+        "korrekt": "2",
+        "korrekteAntwort": "60 Kilogramm"
+
+    },
+    {
+        "frage": "An welchem Tag war 2019 der 'Earth Overshoot Day' - also der Tag an dem die Menschheit mehr Ressourcen verbraucht hat als die Erde produzieren kann?",
+        "a1": "3. April",
+        "a2": "18. August",
+        "a3": "29. Juli",
+        "korrekt": "3",
+        "korrekteAntwort": "29. Juli"
+
+    },
+    {
+        "frage": "Welches dieser Länder ist pro Kopf gemessen 'Elektroschrott-Weltmeister' ?",
+        "a1": "China",
+        "a2": "Norwegen",
+        "a3": "USA",
+        "korrekt": "2",
+        "korrekteAntwort": "Norwegen"
+
+    }
+]
+
+let endText = [
+
+    {
+        "Name": "Donald Trump",
+        "Bild": "<img class='ergebnis-bild' src='trump.jpg'>",
+        "Text": "Tremendous job! Du hast zwar absolut keine Ahnung von dieser Materie, das hält dich aber nicht davon ab den Experten zu mimen. Während du bei deiner Pressekonferenz das linkslinke Journalistenpack zusammenstauchst, welches es wagt dich mit seinen „Fakten“ zu belästigen, versuchen deine Berater sich nicht vor Lachen in die Hose zu machen. Great success!"
+
+    },
+
+    {
+        "Name": "Influencer",
+        "Bild": "<img class='ergebnis-bild' src='influencer.jpg'>",
+        "Text": "Gratuliere, du bist zumindest nicht komplett ahnungslos. Du kaufst nachhaltige Biomode, klärst deine Follower über die CO2-Belastung einer nicht-veganen Ernährung auf und pendelst in der kalten Jahreszeit gern zwischen Berlin und Tel Aviv, um der Umwelt zu liebe weniger zu heizen. "
+
+    },
+
+    {
+        "Name": "Greta Thunberg",
+        "Bild": "<img class='ergebnis-bild' src='greta.jpg'>",
+        "Text": "Du bist jung, wütend, trägst – zumindest irgendwo am Körper – Zöpfe und kennst die Materie im Schlaf! Umweltsünder und Präsidenten der United States of America strafst du mit Zitaten aus den aktuellsten wissenschaftlichen Publikationen und natürlich deinen apathischen Laser-Blicken. How dare you? "
+
+    },
+
+]
+
+let shareMenu = '<div class="sharemenu"><div class="share-text">Teile es mit</div> <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.dailyimpact.earth/home/&amp;quote=Wusstest du, dass jeder Mensch pro Woche Plastik in der Menge von einer Kreditkarte zu sich nimmt?" class="fbc-has-badge fbc-UID_8"><i class="fab fa-facebook-square" aria-hidden="true"></i></a><a href="whatsapp://send?text=Wusstest du, dass jeder Mensch pro Woche Plastik in der Menge von einer Kreditkarte zu sich nimmt? --> https://www.dailyimpact.earth/home/" data-action="share/whatsapp/share" title="Share on whatsapp"><i class="fab fa-whatsapp-square" aria-hidden="true"></i></a><a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=http://www.dailyimpact.earth/home/&amp;title=Wusstest du, dass jeder Mensch pro Woche Plastik in der Menge von einer Kreditkarte zu sich nimmt?"><i class="fab fa-linkedin" aria-hidden="true"></i></a><a href="http://twitter.com/share?text=Wusstest du, dass jeder Mensch pro Woche Plastik in der Menge von einer Kreditkarte zu sich nimmt?&amp;url=http://www.dailyimpact.earth/home/&amp;hashtags=createimpact,impact,noplastic"><i class="fab fa-twitter-square" aria-hidden="true"></i></a></div>'
