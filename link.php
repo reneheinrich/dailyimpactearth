fa72b557c19c8f3ae93206edbf2ed4d112424b6a
René Heinrich <rene@halphbaqed.com>
2020-04-10 13:23:23 +0200

    Testing Share Api

--- link.php
+++ link.php
@@ -183,16 +183,26 @@
 </svg>
 <script>
     const shareButton = document.querySelector('.share-button');
-    const shareDialog = document.querySelector('.share-dialog');
-    const closeButton = document.querySelector('.close-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
 
-    shareButton.addEventListener('click', event => {
-    shareDialog.classList.add('is-open');
-    });
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'Daily Impact',
+      url: 'https://dailyimpact.earth/home/'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    } else {
+        shareDialog.classList.add('is-open');
+    }
+});
 
-    closeButton.addEventListener('click', event => {
-    shareDialog.classList.remove('is-open');
-    });
+closeButton.addEventListener('click', event => {
+  shareDialog.classList.remove('is-open');
+});
 </script>
 </body>
 </html>
