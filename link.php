1cc1c034c54dc30fc0e6b24c8ba6ecfd4a6c079a
René Heinrich <rene@halphbaqed.com>
2020-04-10 14:21:43 +0200

    WEb sharing test

--- link.php
+++ link.php
@@ -78,7 +78,7 @@
         z-index: 2;
         }
 
-        header {
+        .share-header {
         display: flex;
         justify-content: space-between;
         margin-bottom: 20px;
@@ -119,90 +119,130 @@
         }
 </style>
     <body>
-    <div class="share-dialog">
-  <header>
-    <h3 class="dialog-title">Share this pen</h3>
-    <button class="close-button"><svg><use href="#close"></use></svg></button>
-  </header>
-  <div class="targets">
-    <a class="button">
-      <svg>
-        <use href="https://www.facebook.com/sharer/sharer.php?u=https://www.dailyimpact.earth/home/&quote=Wusstest+du%2C+dass+jeder+Mensch+pro+Woche+Plastik+in+der+Menge+von+einer+Kreditkarte+zu+sich+nimmt%3F"></use>
-      </svg>
-      <span>Facebook</span>
-    </a>
-    
-    <a class="button">
-      <svg>
-        <use href="http://twitter.com/share?text=Wusstest+du%2C+dass+jeder+Mensch+pro+Woche+Plastik+in+der+Menge+von+einer+Kreditkarte+zu+sich+nimmt%3F&url=http://www.dailyimpact.earth/home/&hashtags=createimpact,impact,noplastic"></use>
-      </svg>
-      <span>Twitter</span>
-    </a>
-    
-    <a class="button">
-      <svg>
-        <use href="https://www.linkedin.com/shareArticle?mini=true&url=http://www.dailyimpact.earth/home/&title=Wusstest+du%2C+dass+jeder+Mensch+pro+Woche+Plastik+in+der+Menge+von+einer+Kreditkarte+zu+sich+nimmt%3F"></use>
-      </svg>
-      <span>LinkedIn</span>
-    </a>
-    
-    <a class="button">
-      <svg>
-        <use href="mailto://#"></use>
-      </svg>
-      <span>Email</span>
-    </a>
-  </div>
-  <div class="link">
-    <div class="pen-url">https://dailyimpact.earth/home/</div>
-    <button class="copy-link">Copy Link</button>
-  </div>
-</div>
+        <div class="share-dialog">
+            <div class="share-header">
+                <h3 class="dialog-title">Teile es mit</h3>
+                <button class="close-button"><svg><use href="#close"></use></svg></button>
+            </div>
+            <div class="targets">
+                <a class="button" href="https://www.facebook.com/sharer/sharer.php?u=https://www.dailyimpact.earth/home/&quote=Wusstest+du%2C+dass+jeder+Mensch+pro+Woche+Plastik+in+der+Menge+von+einer+Kreditkarte+zu+sich+nimmt%3F">
+                <svg>
+                    <use href="#facebook"></use>
+                </svg>
+                <span>Facebook</span>
+                </a>
+                
+                <a class="button" href="http://twitter.com/share?text=Wusstest+du%2C+dass+jeder+Mensch+pro+Woche+Plastik+in+der+Menge+von+einer+Kreditkarte+zu+sich+nimmt%3F&url=http://www.dailyimpact.earth/home/&hashtags=createimpact,impact,noplastic">
+                <svg>
+                    <use href="#twitter"></use>
+                </svg>
+                <span>Twitter</span>
+                </a>
+                
+                <a class="button" href="https://www.linkedin.com/shareArticle?mini=true&url=http://www.dailyimpact.earth/home/&title=Wusstest+du%2C+dass+jeder+Mensch+pro+Woche+Plastik+in+der+Menge+von+einer+Kreditkarte+zu+sich+nimmt%3F">
+                <svg>
+                    <use href="#linkedin"></use>
+                </svg>
+                <span>LinkedIn</span>
+                </a>
+                
+                <a class="button" href="mailto:?Subject=Hello%20againWusstest+du%2C+dass+jeder+Mensch+pro+Woche+Plastik+in+der+Menge+von+einer+Kreditkarte+zu+sich+nimmt%3F&body=Informiere+dich+auf+https%3A%2F%2Fdailyimpact.earth%2Fhome%2F+%C3%BCber+interessante+Fakten+rund+um+Nachhaltigkeit+und+Klimaschutz.">
+                <svg>
+                    <use href="#email"></use>
+                </svg>
+                <span>Email</span>
+                </a>
+            </div>
+            <div class="link">
+                <div class="pen-url">https://dailyimpact.earth/home/</div>
+                <button class="copy-link">Copy Link</button>
+            </div>
+        </div>
 
-<button class="share-button" type="button" title="Share this article">
-  <svg>
-    <use href="#share-icon"></use>
-  </svg>
-  <span>Share</span>
-</button>
+        <button class="share-button" type="button" title="Teile diesen Fakt">
+            <svg>
+                <use href="#share-icon"></use>
+            </svg>
+            <span>Teile diese Seite</span>
+        </button>
 
-<svg class="hidden">
-  <defs>
-    <symbol id="share-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></symbol>
-    
-    <symbol id="facebook" viewBox="0 0 24 24" fill="#3b5998" stroke="#3b5998" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></symbol>
-    
-    <symbol id="twitter" viewBox="0 0 24 24" fill="#1da1f2" stroke="#1da1f2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></symbol>
-    
-    <symbol id="email" viewBox="0 0 24 24" fill="#777" stroke="#fafafa" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></symbol>
-    
-    <symbol id="linkedin" viewBox="0 0 24 24" fill="#0077B5" stroke="#0077B5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></symbol>
-    
-    <symbol id="close" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></symbol>
-  </defs>
-</svg>
-<script>
-    const shareButton = document.querySelector('.share-button');
-const shareDialog = document.querySelector('.share-dialog');
-const closeButton = document.querySelector('.close-button');
+        <svg class="hidden">
+        <defs>
+            <symbol id="share-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></symbol>
+            
+            <symbol id="facebook" viewBox="0 0 24 24" fill="#3b5998" stroke="#3b5998" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></symbol>
+            
+            <symbol id="twitter" viewBox="0 0 24 24" fill="#1da1f2" stroke="#1da1f2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></symbol>
+            
+            <symbol id="email" viewBox="0 0 24 24" fill="#777" stroke="#fafafa" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></symbol>
+            
+            <symbol id="linkedin" viewBox="0 0 24 24" fill="#0077B5" stroke="#0077B5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></symbol>
+            
+            <symbol id="close" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></symbol>
+        </defs>
+        </svg>
+        <script>
 
-shareButton.addEventListener('click', event => {
-  if (navigator.share) { 
-   navigator.share({
-      title: 'Daily Impact',
-      url: 'https://dailyimpact.earth/home/'
-    }).then(() => {
-      console.log('Thanks for sharing!');
-    })
-    .catch(console.error);
-    } else {
-        shareDialog.classList.add('is-open');
-    }
-});
+            function fallbackCopyTextToClipboard(text) {
+                var textArea = document.createElement("textarea");
+                textArea.value = text;
 
-closeButton.addEventListener('click', event => {
-  shareDialog.classList.remove('is-open');
-});
-</script>
-</body>
+                // Avoid scrolling to bottom
+                textArea.style.top = "0";
+                textArea.style.left = "0";
+                textArea.style.position = "fixed";
+
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+
+                try {
+                    var successful = document.execCommand('copy');
+                    var msg = successful ? 'successful' : 'unsuccessful';
+                    console.log('Fallback: Copying text command was ' + msg);
+                } catch (err) {
+                    console.error('Fallback: Oops, unable to copy', err);
+                }
+
+                document.body.removeChild(textArea);
+            }
+            function copyTextToClipboard(text) {
+                if (!navigator.clipboard) {
+                    fallbackCopyTextToClipboard(text);
+                    return;
+                }
+                navigator.clipboard.writeText(text).then(function() {
+                    console.log('Async: Copying to clipboard was successful!');
+                }, function(err) {
+                    console.error('Async: Could not copy text: ', err);
+                });
+            }
+
+            const shareButton = document.querySelector('.share-button');
+            const shareDialog = document.querySelector('.share-dialog');
+            const closeButton = document.querySelector('.close-button');
+            const copyButton  = document.querySelector('.copy-link');
+
+            shareButton.addEventListener('click', event => {
+            if (navigator.share) { 
+            navigator.share({
+                title: 'Daily Impact',
+                url: 'https://dailyimpact.earth/home/'
+                }).then(() => {
+                console.log('Thanks for sharing!');
+                })
+                .catch(console.error);
+                } else {
+                    shareDialog.classList.add('is-open');
+                }
+            });
+
+            closeButton.addEventListener('click', event => {
+                shareDialog.classList.remove('is-open');
+            });
+            copyButton.addEventListener('click', event => {
+                copyTextToClipboard('https://dailyimpact.earth/home/');
+            });
+        </script>
+    </body>
 </html>
